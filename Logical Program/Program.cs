e06db583abdd4b22a91b6225c10fc3e4ac9a910a
Aftab-1998 <alam41976@gmail.com>
2021-12-03 21:17:03 +0530

    [Aftab] Add ; Added Fabicon Series

---
+++ Logical Program/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LogicalPrograms
+{
+    class FibonacciSeries
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter the program number which is to be executed \n 1. Fibonacci Series \n 2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FibonacciSeries series = new FibonacciSeries();
+                        series.Equals(new FibonacciSeries());
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
